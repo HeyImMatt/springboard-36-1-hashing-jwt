202999d417c72405d92e0a35edb478dae9b64485
heyimmatt <mrmcfarland@gmail.com>
2020-10-12 16:02:55 -0700

    Add message routes

--- routes/messages.js
+++ routes/messages.js
@@ -5,6 +5,8 @@ const router = new Router();
 const User = require("../models/user");
 const { SECRET_KEY } = require("../config");
 const ExpressError = require("../expressError");
+const Message = require("../models/message");
+const { ensureLoggedIn, ensureCorrectUser } = require("../middleware/auth");
 
 /** GET /:id - get detail of message.
  *
@@ -19,6 +21,21 @@ const ExpressError = require("../expressError");
  *
  **/
 
+router.get("/:id", ensureCorrectUser, async (req, res, next) => {
+  try {
+    let username = req.user.username;
+    const message = await Message.get(req.params.id);
+
+    if (message.to_user.username !== username && message.from_user.username !== username) {
+      throw new ExpressError("Cannot read this message", 401);
+    }
+
+    return res.json({message})
+  } catch (err) {
+    return next(err);
+  }
+})
+
 
 /** POST / - post message.
  *
@@ -26,6 +43,20 @@ const ExpressError = require("../expressError");
  *   {message: {id, from_username, to_username, body, sent_at}}
  *
  **/
+
+router.post("/", ensureCorrectUser, async (req, res, next) => {
+  try {
+    let message = await Message.create({
+      from_username: req.user.username,
+      to_username: req.body.to_username,
+      body: req.body.body
+    });
+
+    return res.json({message});
+  } catch (err) {
+    return next(err);
+  }
+})
 
 
 /** POST/:id/read - mark message as read:
@@ -35,5 +66,20 @@ const ExpressError = require("../expressError");
  * Make sure that the only the intended recipient can mark as read.
  *
  **/
+
+router.post("/:id/read", ensureCorrectUser, async (req, res, next) => {
+  try {
+    let username = req.user.username;
+    let msg = await Message.get(req.params.id);
+
+    if (msg.to_user.username !== username) {
+      throw new ExpressError('Cannot mark message as read', 401)
+    }
+    let message = await Message.markRead(req.params.id);
+    return res.json({message});
+  } catch (err) {
+    return next(err);
+  }
+})
 
 module.exports = router;
